63604bfa86068b4c72fdac777df21fbc155d110e
peihsinsu <simonsu.mail@gmail.com>
2013-10-03 21:17:37 +0800

    refactory of helloword sample

--- helloworld/client.js
+++ helloworld/client.js
@@ -1,4 +1,6 @@
 <script src="http://localhost:8088/socket.io/socket.io.js"></script>
+<script src="http://code.jquery.com/jquery-2.0.2.min.js"></script>
+
 <script>
 var socket = io.connect('http://localhost:8088');
 socket.on('news', function (data) {
--- helloworld/server.js
+++ helloworld/server.js
@@ -1,18 +1,22 @@
-var io = require('socket.io').listen(8088);
+//var io = require('socket.io').listen(8088)
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(8088); 
+
+function handler (req, res) {
+  res.writeHead(200);
+  res.end(fs.readFileSync(__dirname + '/client.js'));
+}
 
 console.log('SocketIO started....');
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+  setInterval(function(){
+  socket.emit('news', { hello: 'world ' + new Date()  });
+  }, 1000);
   socket.on('my other event', function (data) {
     console.log(data);
   });
 });
 
-var http = require('http')
-  , fs = require('fs')
-
-console.log('http server started....');
-http.createServer(function(req, res){
-  console.log('access http server...');
-  res.end(fs.readFileSync(__dirname + '/client.js'));
-}).listen(8080);

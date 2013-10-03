bc059ee32827f2f55ae2361f8e2606604d95e595
peihsinsu <simonsu.mail@gmail.com>
2013-10-03 21:17:02 +0800

    refactory code

--- socketioDemo/app.js
+++ socketioDemo/app.js
@@ -1,29 +1,29 @@
 var app = require('http').createServer(handler)
-	, io = require('socket.io').listen(app)
-	, fs = require('fs');
+  , io = require('socket.io').listen(app)
+  , fs = require('fs');
 app.listen(8088);
 
 function handler (req, res) { 
-	fs.readFile(__dirname + '/chat.html', function (err, data) {
-		if (err) { 
-			res.writeHead(500);
-			return res.end('Error loading chat.html'); 
-		}
-		res.writeHead(200);
-		res.end(data); 
-	});
+  fs.readFile(__dirname + '/chat.html', function (err, data) {
+    if (err) { 
+      res.writeHead(500);
+      return res.end('Error loading chat.html'); 
+    }
+    res.writeHead(200);
+    res.end(data); 
+  });
 }
 io.sockets.on('connection', function (socket) {
-	socket.on('addme',function(username) {
-		socket.username = username;
-		socket.emit('chat', 'SERVER', 'You have connected'); 
-		socket.broadcast.emit('chat', 'SERVER', username + ' is on deck');
-	});
-	socket.on('sendchat', function(data) { 
-		io.sockets.emit('chat', socket.username, data);
-	});
+  socket.on('addme',function(username) {
+    socket.username = username;
+    socket.emit('chat', 'SERVER', 'Hello '+username+', You have connected!'); 
+    socket.broadcast.emit('chat', 'SERVER', username + ' is on deck');
+  });
+  socket.on('sendchat', function(data) { 
+    io.sockets.emit('chat', socket.username, data);
+  });
 
-	socket.on('disconnect', function() {
-		io.sockets.emit('chat', 'SERVER', socket.username + ' has left!!!');
-	});
+  socket.on('disconnect', function() {
+    io.sockets.emit('chat', 'SERVER', socket.username + ' has left!!!');
+  });
 });

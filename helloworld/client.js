990e1ad8e27147bd66caa69a689b6f9cee9df1d0
peihsinsu <simonsu.mail@gmail.com>
2013-10-04 16:38:20 +0800

    update to display to html client

--- helloworld/client.js
+++ helloworld/client.js
@@ -6,9 +6,13 @@ var socket = io.connect('http://localhost:8088');
 socket.on('news', function (data) {
   console.log(data);
   socket.emit('my other event', { my: 'data' });
+  $('#ts').html(JSON.stringify(data));
 });
 </script>
 
 <h1>
 TEST...
+<div id="ts">
+time will be here...
+</div>
 </h1>
